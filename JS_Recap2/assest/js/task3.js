341557b2382b756a0de6e0e3a5f22dfbf2bca3cf
asadniyazov <bd5SCJWR5@code.edu.az>
2024-04-14 14:07:35 -0700

    second commit

--- JS_Recap2/assest/js/task3.js
+++ JS_Recap2/assest/js/task3.js
@@ -2,7 +2,6 @@
  const inp2=document.getElementById("inp2")
  const btn=document.getElementById("btn")
  const div=document.getElementById("main_div")
-//const obj=localStorage.getItem("key") ? JSON.parse(localStorage.getItem("key")) :{inp1:"",inp2:""};
 let arr=localStorage.getItem("key") ? JSON.parse(localStorage.getItem("key")) :[]
 console.log(arr);
 arr.forEach(x => {
@@ -12,10 +11,10 @@ arr.forEach(x => {
     h1.className="h1"
     const p=document.createElement("p")
     p.className="p"
-    div.append(div1)
-    div1.append(h1,p)
     h1.textContent=x.title;
     p.textContent=x.description;
+    div.append(div1)
+    div1.append(h1,p)
  });
 btn.addEventListener("click",function (e) {
     if (inp1.value==="" || inp2.value==="") {
@@ -28,13 +27,10 @@ btn.addEventListener("click",function (e) {
     h1.className="h1"
     const p=document.createElement("p")
     p.className="p"
-    div.append(div1)
-    div1.append(h1,p)
     h1.textContent=inp1.value;
     p.textContent=inp2.value
     arr.push({title:inp1.value,description:inp2.value})
     localStorage.setItem("key",JSON.stringify(arr))
+    div.append(div1)
+    div1.append(h1,p)
  })
-//  console.log(obj.length);
-//  console.log(Object.values(obj)[1]);
-//  localStorage.clear()
